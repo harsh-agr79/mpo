1a023ab248d3f579dd1e7f90541bc2ee572ad040
iNeatz <sahilshrestha77@gmail.com>
2025-05-05 18:44:07 +0545

    category filter tabs

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
+use App\Models\Category;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListProducts extends ListRecords
 {
@@ -15,5 +18,20 @@ class ListProducts extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [
+            'all' => Tab::make('All'),
+        ];
+        $categories = Category::all();
+
+        foreach ($categories as $category) {
+            $tabs[$category->id] = Tab::make($category->name)
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('category_id', $category->id));
+        }
+
+        return $tabs;
     }
 }
