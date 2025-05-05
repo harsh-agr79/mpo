6f6db41dc7a0d2ebc0ebbd695e88ad03cf2be2fd
iNeatz <sahilshrestha77@gmail.com>
2025-05-05 16:10:20 +0545

    part infolist

--- app/Filament/Resources/PartResource.php
+++ app/Filament/Resources/PartResource.php
@@ -12,8 +12,12 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -74,6 +78,25 @@ class PartResource extends Resource
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                ViewAction::make()
+                    ->modalHeading(fn($record) => 'Part: ' . ucfirst($record->id))
+                    ->modalSubmitAction(false)
+                    ->modalCancelActionLabel('Close')
+                    ->infolist([
+                        Section::make()
+                            ->schema([
+
+                                TextEntry::make('name')->label('PART NAME'),
+                                ImageEntry::make('image')->label('IMAGE')->visible(fn($record) => filled($record->image)),
+                                TextEntry::make('product.name')->label('PRODUCT')->visible(fn($record) => filled($record->product)),
+                                TextEntry::make('open_balance')->label('OPEN BALANCE')->visible(fn($record) => filled($record->open_balance)),
+                                TextEntry::make('created_at')->label('CREATED_AT'),
+                                TextEntry::make('updated_at')->label('UPDATED_AT'),
+                                TextEntry::make('deleted_at')->label('DELETED_AT')->visible(fn($record) => filled($record->deleted_at)),
+
+                            ])
+                            ->columns(2),
+                    ]),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ForceDeleteAction::make(),
