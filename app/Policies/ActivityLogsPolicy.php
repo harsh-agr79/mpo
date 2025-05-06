50a0c97be07da50598749664324733fe682b6177
iNeatz <sahilshrestha77@gmail.com>
2025-05-06 22:11:20 +0545

    activity logs view policy

---
+++ app/Policies/ActivityLogsPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ActivityLog;
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ActivityLogsPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(Admin $admin): bool
+    {
+        if($admin->hasPermissionTo('View Activity Logs')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(Admin $admin, ActivityLog $activityLog): bool
+    {
+        if($admin->hasPermissionTo('View Activity Logs')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(Admin $admin): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(Admin $admin, ActivityLog $activityLog): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(Admin $admin, ActivityLog $activityLog): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(Admin $admin, ActivityLog $activityLog): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(Admin $admin, ActivityLog $activityLog): bool
+    {
+        return false;
+    }
+}
