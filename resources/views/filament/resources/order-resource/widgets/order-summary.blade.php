78ff8b878f2e666ebd9c4b283ffed5056cf4546f
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-26 22:40:49 +0545

    category summary

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -1,13 +1,12 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-    <div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="text-xs">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
             <div class="space-y-2">
                 <div><strong>Date:</strong> {{ $order->date }}</div>
                 <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
                 <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
                 <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
-                
             </div>
             <div class="space-y-2">
                 <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
