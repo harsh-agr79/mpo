b135d20d01837d84c6a3700f3a60b6ab17c77088
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-27 23:56:28 +0545

    material invoice complete

--- app/Filament/Resources/MaterialInvoiceResource/Pages/EditMaterialInvoice.php
+++ app/Filament/Resources/MaterialInvoiceResource/Pages/EditMaterialInvoice.php
@@ -13,7 +13,26 @@ class EditMaterialInvoice extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
+    }
+
+     public function getTitle(): string {
+        return '';
+        // Ensure nothing is rendered
+    }
+
+    public function mount( $record ): void {
+        parent::mount( $record );
+
+        $user = Auth::user();
+
+        if (
+            $user->hasPermissionTo( 'Material View First' ) &&
+            is_null( $this->record->seenby )
+        ) {
+            $this->record->seenby = $user->id;
+            $this->record->save();
+        }
     }
 }
