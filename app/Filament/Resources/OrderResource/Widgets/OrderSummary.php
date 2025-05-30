4927c522872cba9fa6f791510b874ecb0a10070c
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:19:01 +0545

    collapse

--- app/Filament/Resources/OrderResource/Widgets/OrderSummary.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderSummary.php
@@ -4,6 +4,11 @@ namespace App\Filament\Resources\OrderResource\Widgets;
 
 use Filament\Widgets\Widget;
 use App\Models\Order;
+use Filament\Infolists\Components\InfoList;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\TextEntry;
+use Illuminate\Support\HtmlString;
+
 
 class OrderSummary extends Widget
 {
@@ -29,4 +34,36 @@ class OrderSummary extends Widget
     }
 
     protected int | string | array $columnSpan = 'full';
+
+     public function getInfoList(): InfoList
+    {
+        return InfoList::make()
+            ->columns(2)
+            ->schema([
+                // Always-visible fields
+                TextEntry::make('user.name')->label('Name'),
+                TextEntry::make('date')->label('Date'),
+
+                // Collapsible fields (handled in Blade view with Alpine.js)
+                Group::make([
+                    TextEntry::make('user.shop_name')->label('Shop'),
+                    TextEntry::make('user.contact')->label('Phone no.'),
+                    TextEntry::make('user.address')->label('Address'),
+                    TextEntry::make('user.tax_no')->label('Pan no.'),
+                    TextEntry::make('orderid')->label('Order ID'),
+                    TextEntry::make('miti')
+                        ->label('Miti')
+                        ->default(fn () => getNepaliDate($this->order->date)),
+                ])
+                ->extraAttributes(['x-show' => 'open'])
+                ->columnSpanFull(), // Keeps layout clean
+            ]);
+    }
+
+    public function render(): \Illuminate\Contracts\View\View
+    {
+        return view(static::$view, [
+            'infoList' => $this->getInfoList(),
+        ]);
+    }
 }
--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -1,25 +1,8 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div x-data="{ open: false }">
-            {{-- Always-visible info list --}}
-            <x-filament::info-list :columns="2">
-                <x-filament::info-list-item label="Name" :value="$order->user->name" />
-                <x-filament::info-list-item label="Date" :value="$order->date" />
-            </x-filament::info-list>
-
-            {{-- Collapsible info list --}}
-            <div x-show="open">
-                <x-filament::info-list :columns="2" class="mt-2">
-                    <x-filament::info-list-item label="Shop" :value="$order->user->shop_name" />
-                    <x-filament::info-list-item label="Miti" :value="getNepaliDate($order->date)" />
-
-                    <x-filament::info-list-item label="Phone no." :value="$order->user->contact" />
-                    <x-filament::info-list-item label="Order ID" :value="$order->orderid" />
-
-                    <x-filament::info-list-item label="Address" :value="$order->user->address" />
-                    <x-filament::info-list-item label="Pan no." :value="$order->user->tax_no" />
-                </x-filament::info-list>
-            </div>
+            {{-- Always-visible fields --}}
+            {!! $infoList->columns(2)->toHtml() !!}
 
             {{-- Toggle Button --}}
             <div class="mt-2">
