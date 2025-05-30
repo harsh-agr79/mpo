2ef68e71fdc5e5fc773ba77ff8a0b49a5e16cd26
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:10:00 +0545

    collapse

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -2,31 +2,25 @@
     <x-filament::section>
         <div x-data="{ open: false }" class="text-xs">
             <div class="grid grid-cols-2 gap-2">
-                <!-- Always visible top row -->
-                <div>
-                    <div><strong>Name:</strong> {{ $order->user->name }}</div>
-                </div>
-                <div>
-                    <div><strong>Date:</strong> {{ $order->date }}</div>
-                </div>
+                <!-- Always visible fields -->
+                <div><strong>Name:</strong> {{ $order->user->name }}</div>
+                <div><strong>Date:</strong> {{ $order->date }}</div>
 
-                <!-- Collapsible section -->
+                <!-- Collapsible fields -->
                 <template x-if="open">
-                    <div class="col-span-2 grid grid-cols-2 gap-2">
-                        <div class="space-y-2">
-                            <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
-                            <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
-                            <div><strong>Address:</strong> {{ $order->user->address }}</div>
-                            <div><strong>Pan no.:</strong> {{ $order->user->tax_no }}</div>
-                        </div>
-                        <div class="space-y-2">
-                            <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
-                            <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
-                        </div>
-                    </div>
+                    <template>
+                        <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
+                        <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
+
+                        <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
+                        <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
+
+                        <div><strong>Address:</strong> {{ $order->user->address }}</div>
+                        <div><strong>Pan no.:</strong> {{ $order->user->tax_no }}</div>
+                    </template>
                 </template>
 
-                <!-- Toggle button -->
+                <!-- Toggle button spans both columns -->
                 <div class="col-span-2">
                     <button @click="open = !open"
                         class="text-blue-600 hover:underline focus:outline-none text-sm">
