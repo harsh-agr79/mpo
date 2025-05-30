565668ca93cf0c04d3ad8011a2fafd9d658cdc82
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:06:19 +0545

    collapse

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -4,15 +4,15 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
             <div class="space-y-2">
                 <div><strong>Name:</strong> {{ $order->user->name }}</div>
-                <div><strong>Date:</strong> {{ $order->date }}</div>
-                <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
-                <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
-            </div>
-            <div class="space-y-2">
-                <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
                 <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
                 <div><strong>Address:</strong> {{ $order->user->address }}</div>
                 <div><strong>Pan no.:</strong> {{ $order->user->tax_no }}</div>
+                <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
+            </div>
+            <div class="space-y-2">
+                <div><strong>Date:</strong> {{ $order->date }}</div>
+                <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
+                <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
             </div>
         </div>
     </div>
