ae068c9ff4c08445c2b6bfe921770e896af4bc95
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:09:12 +0545

    collapse

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -1,20 +1,40 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-    <div class="text-xs">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
-            <div class="space-y-2">
-                <div><strong>Name:</strong> {{ $order->user->name }}</div>
-                <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
-                 <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
-                <div><strong>Address:</strong> {{ $order->user->address }}</div>
-                <div><strong>Pan no.:</strong> {{ $order->user->tax_no }}</div>
-            </div>
-            <div class="space-y-2">
-                <div><strong>Date:</strong> {{ $order->date }}</div>
-                <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
-                <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
+        <div x-data="{ open: false }" class="text-xs">
+            <div class="grid grid-cols-2 gap-2">
+                <!-- Always visible top row -->
+                <div>
+                    <div><strong>Name:</strong> {{ $order->user->name }}</div>
+                </div>
+                <div>
+                    <div><strong>Date:</strong> {{ $order->date }}</div>
+                </div>
+
+                <!-- Collapsible section -->
+                <template x-if="open">
+                    <div class="col-span-2 grid grid-cols-2 gap-2">
+                        <div class="space-y-2">
+                            <div><strong>Shop:</strong> {{ $order->user->shop_name }}</div>
+                            <div><strong>Phone no.:</strong> {{ $order->user->contact }}</div>
+                            <div><strong>Address:</strong> {{ $order->user->address }}</div>
+                            <div><strong>Pan no.:</strong> {{ $order->user->tax_no }}</div>
+                        </div>
+                        <div class="space-y-2">
+                            <div><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</div>
+                            <div><strong>Order ID:</strong> {{ $order->orderid }}</div>
+                        </div>
+                    </div>
+                </template>
+
+                <!-- Toggle button -->
+                <div class="col-span-2">
+                    <button @click="open = !open"
+                        class="text-blue-600 hover:underline focus:outline-none text-sm">
+                        <span x-show="!open">Show More</span>
+                        <span x-show="open">Show Less</span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-</x-filament::section>
+    </x-filament::section>
 </x-filament-widgets::widget>
