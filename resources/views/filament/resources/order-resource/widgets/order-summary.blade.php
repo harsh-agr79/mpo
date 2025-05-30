66d9b082011bead7b373dbde3f36f7e12f45e158
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:30:33 +0545

    collapse

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -5,30 +5,22 @@
                 <tbody>
                     {{-- Always visible row --}}
                     <tr>
-                        <td class="w-1/4"><strong>Name:</strong></td>
-                        <td class="w-1/4">{{ $order->user->name }}</td>
-                        <td class="w-1/4"><strong>Date:</strong></td>
-                        <td class="w-1/4">{{ $order->date }}</td>
+                        <td class="w-1/4"><strong>Name:</strong> {{ $order->user->name }}</td>
+                        <td class="w-1/4"><strong>Date:</strong> {{ $order->date }}</td>
                     </tr>
 
                     {{-- Toggleable rows --}}
                     <tr x-show="open">
-                        <td><strong>Shop:</strong></td>
-                        <td>{{ $order->user->shop_name }}</td>
-                        <td><strong>Miti:</strong></td>
-                        <td>{{ getNepaliDate($order->date) }}</td>
+                        <td><strong>Shop:</strong> {{ $order->user->shop_name }}</td>
+                        <td><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</td>
                     </tr>
                     <tr x-show="open">
-                        <td><strong>Phone no.:</strong></td>
-                        <td>{{ $order->user->contact }}</td>
-                        <td><strong>Order ID:</strong></td>
-                        <td>{{ $order->orderid }}</td>
+                        <td><strong>Phone no.:</strong> {{ $order->user->contact }}</td>
+                        <td><strong>Order ID:</strong> {{ $order->orderid }}</td>
                     </tr>
                     <tr x-show="open">
-                        <td><strong>Address:</strong></td>
-                        <td>{{ $order->user->address }}</td>
-                        <td><strong>Pan no.:</strong></td>
-                        <td>{{ $order->user->tax_no }}</td>
+                        <td><strong>Address:</strong> {{ $order->user->address }}</td>
+                        <td><strong>Pan no.:</strong> {{ $order->user->tax_no }}</td>
                     </tr>
                 </tbody>
             </table>
