c464221924bee3539d3655cc325b0cab14368dc3
harsh-agr79 <agrharsh7932@gmail.com>
2025-05-30 17:33:52 +0545

    collapse

--- resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
+++ resources/views/filament/resources/order-resource/widgets/order-summary.blade.php
@@ -3,28 +3,13 @@
         <div x-data="{ open: false }" class="text-xs w-full">
             <table class="w-full table-auto text-center border-separate border-spacing-y-2">
                 <tbody>
-                    <!-- Always visible row -->
+                    {{-- Always visible row --}}
                     <tr>
-                        <td class="w-1/4 text-left">
-                            <strong>Name:</strong> {{ $order->user->name }}
-                        </td>
-                        <td class="w-1/4 text-left">
-                            <strong>Date:</strong> {{ $order->date }}
-                        </td>
-                        <td class="w-1/2 text-right pr-4" colspan="2">
-                            <button @click="open = !open" class="focus:outline-none inline-flex items-center justify-end w-full">
-                                <svg 
-                                    x-bind:class="open ? 'transform rotate-180' : 'transform rotate-0'"
-                                    class="w-5 h-5 transition-transform duration-300 text-gray-600"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                >
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                                </svg>
-                            </button>
-                        </td>
+                        <td class="w-1/4"><strong>Name:</strong> {{ $order->user->name }}</td>
+                        <td class="w-1/4"><strong>Date:</strong> {{ $order->date }}</td>
                     </tr>
 
-                    <!-- Toggleable rows -->
+                    {{-- Toggleable rows --}}
                     <tr x-show="open">
                         <td><strong>Shop:</strong> {{ $order->user->shop_name }}</td>
                         <td><strong>Miti:</strong> {{ getNepaliDate($order->date) }}</td>
@@ -39,6 +24,15 @@
                     </tr>
                 </tbody>
             </table>
+
+            <div class="text-center mt-2">
+                <button 
+                    @click="open = !open"
+                    class="text-blue-500 hover:underline text-sm"
+                >
+                    <span x-text="open ? 'Show less' : 'Show more'"></span>
+                </button>
+            </div>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
