37e5301857134b533a63efd74a79e6e4a15d89da
harsh-agr79 <agrharsh7932@gmail.com>
2025-06-10 19:30:54 +0545

    product image default array

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -96,19 +96,19 @@ class ProductResource extends Resource
                     ->reorderable()
                     ->addButtonLabel('Add Specification'),
                
-                FileUpload::make('image')
-                    ->required()
-                    ->directory('products')
-                    ->image()
-                    ->maxSize(2048)
-                    ->acceptedFileTypes(['image/svg', 'image/png', 'image/jpg', 'image/jpeg', 'image/webp'])
-                    ->label('Image'),
-                FileUpload::make('image_2')
-                    ->directory('products')
-                    ->image()
-                    ->maxSize(2048)
-                    ->acceptedFileTypes(['image/svg', 'image/png', 'image/jpg', 'image/jpeg', 'image/webp'])
-                    ->label('Additional Image'),
+                // FileUpload::make('image')
+                //     ->required()
+                //     ->directory('products')
+                //     ->image()
+                //     ->maxSize(2048)
+                //     ->acceptedFileTypes(['image/svg', 'image/png', 'image/jpg', 'image/jpeg', 'image/webp'])
+                //     ->label('Image'),
+                // FileUpload::make('image_2')
+                //     ->directory('products')
+                //     ->image()
+                //     ->maxSize(2048)
+                //     ->acceptedFileTypes(['image/svg', 'image/png', 'image/jpg', 'image/jpeg', 'image/webp'])
+                //     ->label('Additional Image'),
                  Repeater::make('images')
                     ->label('Product Images')
                     ->schema([
@@ -117,8 +117,10 @@ class ProductResource extends Resource
                             ->image()
                             ->acceptedFileTypes(['image/svg', 'image/png', 'image/jpg', 'image/jpeg', 'image/webp'])
                             ->imagePreviewHeight('100')
+                            ->required()
                             // ->preserveFilenames(),
                     ])
+                    ->minItems(2)
                     // ->addButtonLabel('Add Image')
                     ->columns(1),
                 Textarea::make('details')
