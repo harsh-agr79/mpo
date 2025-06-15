829f1ef77114b28ca003b801d7ffea1291ef5b63
harsh-agr79 <agrharsh7932@gmail.com>
2025-06-15 18:39:19 +0545

    changes

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
-Schedule::command('stock:update')->everyFiveMinutes();
+Schedule::command('stock:update')->everyMinute();
