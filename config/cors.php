586f2e6fbef9c020429e873fc0bd5940a6690543
harsh-agr79 <agrharsh7932@gmail.com>
2025-06-25 20:03:30 +0545

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:3000', 'https://mp-b2b-c.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
